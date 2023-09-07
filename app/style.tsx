10d2ef86fd31cc880963a3dacea7f77f39f9ee03
dohun0310 <dohun0310@naver.com>
2023-09-07 05:53:14 +0000

    add main page

---
+++ app/style.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import styled from "@emotion/styled";
+
+const StyledHome = styled.main (() => `
+  padding-top: 112px;
+
+  .contents-container{  
+    margin: 0 auto;
+    max-width: 972px;
+    width: 100%;
+    min-height: 100%;
+    height: 100%;
+  }
+`)
+
+export default StyledHome
