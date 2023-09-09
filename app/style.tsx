a1236f536f76667d151d0a07b339651d8962d966
dohun0310 <dohun0310@naver.com>
2023-09-09 20:16:38 +0900

    edit main style

--- app/style.tsx
+++ app/style.tsx
@@ -5,12 +5,10 @@ import styled from "@emotion/styled";
 const StyledHome = styled.main (() => `
   margin: 0 auto;
   max-width: 972px;
+  width: 100%;
   padding-top: 112px;
-  
+
   .page-container {
-    max-width: 972px;
-    padding-top: 112px;
-    width: 100%;
     min-height: 100%;
     height: 100%;
     padding: 0 30px;
